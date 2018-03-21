4bb6b246a7b13fd3d3c490ed75a91d28c8d178f7
Xie, Jihui <jihxie@paypal.com>
2018-03-21 10:02:43 +0800

    Create Heuristics Class, need to finish constructor

--- src/PlayerSkeleton.java
+++ src/PlayerSkeleton.java
@@ -1,3 +1,4 @@
+import java.util.List;
 
 public class PlayerSkeleton {
 
@@ -24,4 +25,71 @@ public class PlayerSkeleton {
 		System.out.println("You have completed "+s.getRowsCleared()+" rows.");
 	}
 	
+}
+
+class Heuristics{
+	private int lineCleared;
+	private List<Hole> holes;
+	private int sumOfAdjacentColumnHeightDifference;
+	private int landingHeight;
+	private int totalHeight;
+	private int rangeOfHeight;
+	
+	public Heuristics(State thisState, State nextState) {
+		lineCleared = nextState.getRowsCleared();
+	}
+	
+	public int getLineCleared() {
+		return lineCleared;
+	}
+	
+	public void setLineCleared(int lineCleared) {
+		this.lineCleared = lineCleared;
+	}
+	
+	public List<Hole> getHoles() {
+		return holes;
+	}
+	
+	public void setHoles(List<Hole> holes) {
+		this.holes = holes;
+	}
+
+	public int getSumOfAdjacentColumnHeightDifference() {
+		return sumOfAdjacentColumnHeightDifference;
+	}
+
+	public void setSumOfAdjacentColumnHeightDifference(int sumOfAdjacentColumnHeightDifference) {
+		this.sumOfAdjacentColumnHeightDifference = sumOfAdjacentColumnHeightDifference;
+	}
+
+	public int getLandingHeight() {
+		return landingHeight;
+	}
+
+	public void setLandingHeight(int landingHeight) {
+		this.landingHeight = landingHeight;
+	}
+
+	public int getTotalHeight() {
+		return totalHeight;
+	}
+
+	public void setTotalHeight(int totalHeight) {
+		this.totalHeight = totalHeight;
+	}
+
+	public int getRangeOfHeight() {
+		return rangeOfHeight;
+	}
+
+	public void setRangeOfHeight(int rangeOfHeight) {
+		this.rangeOfHeight = rangeOfHeight;
+	}
+	
+}
+
+class Hole{
+	private int height;
+	private int weight;
 }
