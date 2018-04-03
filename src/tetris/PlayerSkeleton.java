7e723af46cb4b8010ba2e6d27b7682e3f5c5bb1c
XieJihui <xjh125470939@gmail.com>
2018-04-03 08:33:05 +0800

    no message

--- src/tetris/PlayerSkeleton.java
+++ src/tetris/PlayerSkeleton.java
@@ -3,7 +3,19 @@ package tetris;
 public class PlayerSkeleton {
 
 	private final double[] WEIGHT = {
-			0.24084277294671397, 1.6649915308252572, -3.0351225897331107, -1.3102745688392592, -0.7245259178065809, 0.9883854949501718, -1.0391570392275309, 1.001509772283182, 0.6225281429445286, -0.25979381462637335, -1.2852982903768297, -3.339054331818579, 1.092227646885247
+			1.1970115597151016,
+			-0.6860168418731316,
+			-0.7330712382807072,
+			-1.5889448337710694,
+			-0.25088491435447813,
+			-0.15260870093226603,
+			-0.1072981657226463,
+			-0.9329225453068959,
+			-0.018785370266244772,
+			0.007229001225192391,
+			-1.3826829895407082,
+			-2.7828523911211263,
+			1.0130535867332262
 	};
 	
 	public int pickMove(State s, int[][] legalMoves) {
@@ -24,13 +36,25 @@ public class PlayerSkeleton {
 
 			// Have a copy of the current top
 			int[] currentTop = s.getTop().clone();
+			
+			int testCleared = testMove(orientation, slot, s.getNextPiece(), currentBoard, currentTop,s);
+            if (testCleared < 0) { continue; }
+            
+            NewHeuristic stateEvaluator = new NewHeuristic(currentBoard, s.getTop(), currentTop, testCleared );
 
+            double score = stateEvaluator.getScore(WEIGHT);
+
+            // Updates best score and best move
+            if (score > maxScore) {
+            	maxScore = score;
+                bestMove = i;
+            }
 			//max player choose the maximum value
-			double averageScore = testMove(s, orientation, slot, s.getNextPiece(), currentBoard, currentTop);
-			if(averageScore > maxScore) {
-				maxScore = averageScore;
-				bestMove = i;
-			}
+//			double averageScore = testMove(maxScore,s, orientation, slot, s.getNextPiece(), currentBoard, currentTop);
+//			if(averageScore > maxScore) {
+//				maxScore = averageScore;
+//				bestMove = i;
+//			}
 		}
 
 		if(bestMove == -1)
@@ -56,7 +80,66 @@ public class PlayerSkeleton {
 		System.out.println("You have completed "+s.getRowsCleared()+" rows.");
 	}
 	
-	 private double testMove(State state, int orient, int slot, int nextPiece, int[][] gameBoard,int[] top) {
+	
+	private int testMove(int orient, int slot, int nextPiece, int[][] gameBoard, int[] top, State state) {
+
+        int height = top[slot] - State.getpBottom()[nextPiece][orient][0];
+
+        for (int c = 1; c < State.getpWidth()[nextPiece][orient]; c++) {
+            height = Math.max(height, top[slot + c] - State.getpBottom()[nextPiece][orient][c]);
+        }
+
+        // check if game ended. If game ends, just give -1 as output
+        if (height + State.getpHeight()[nextPiece][orient] >= State.ROWS) { return -1; }
+
+        // for each column in the piece - fill in the appropriate blocks
+        for (int i = 0; i < State.getpWidth()[nextPiece][orient]; i++) {
+
+            // from bottom to top of brick
+            for (int h = height + State.getpBottom()[nextPiece][orient][i]; h < height + State.getpTop()[nextPiece][orient][i]; h++) {
+                gameBoard[h][i + slot] = state.getTurnNumber() + 1;
+            }
+        }
+
+        // adjust top
+        for (int c = 0; c < State.getpWidth()[nextPiece][orient]; c++) {
+            top[slot + c] = height + State.getpTop()[nextPiece][orient][c];
+        }
+
+        int rowsCleared = 0;
+
+        //check for full rows - starting at the top
+        for (int r = height + State.getpHeight()[nextPiece][orient] - 1; r >= height; r--) {
+            //check all columns in the row
+            boolean full = true;
+            for (int c = 0; c < State.COLS; c++) {
+                if (gameBoard[r][c] == 0) {
+                    full = false;
+                    break;
+                }
+            }
+
+            //if the row was full - remove it and slide above stuff down
+            if (full) {
+                rowsCleared++;
+
+                //for each column
+                for (int c = 0; c < State.COLS; c++) {
+
+                    //slide down all bricks
+                    for (int i = r; i < top[c]; i++) {
+                        gameBoard[i][c] = gameBoard[i + 1][c];
+                    }
+                    //lower the top
+                    top[c]--;
+                    while (top[c] >= 1 && gameBoard[top[c] - 1][c] == 0) { top[c]--; }
+                }
+            }
+        }
+        return rowsCleared;
+    }
+	
+	 private double testMove(double maxScore, State state, int orient, int slot, int nextPiece, int[][] gameBoard,int[] top) {
 	    	
  		int height = top[slot] - State.getpBottom()[nextPiece][orient][0];
 
@@ -109,10 +192,10 @@ public class PlayerSkeleton {
      }
      	
      double totalScore = 0;
+     double minScore = Integer.MAX_VALUE;
      
      //test next move after this move is made
      	for(int nextNextPiece = 0; nextNextPiece < State.N_PIECES; nextNextPiece++) {
-     		
      		double secondMaxScore = Integer.MIN_VALUE;
      		
  			for(int i=0;i<State.legalMoves[nextNextPiece].length;i++) {
@@ -130,10 +213,15 @@ public class PlayerSkeleton {
 			
 				double score = testMove(state, nextOrientation, nextSlot, nextNextPiece, currentBoard, currentTop, currentTop);
 				
+				totalScore += score;
+				
 				//max player chooses the minimum value
 				if(score > secondMaxScore) secondMaxScore = score;
+//				//prune
+//				if(secondMaxScore > minScore) break;
 			}
- 			
+// 			if(secondMaxScore < minScore) minScore = secondMaxScore;
+// 			if(minScore < maxScore) break;
  			totalScore += secondMaxScore;
      	}
      	
@@ -196,8 +284,8 @@ public class PlayerSkeleton {
 	            }
 	        }
 
-			Heuristic stateEvaluator = new Heuristic(gameBoard, lastTop, top, rowsCleared);
-	        double score = stateEvaluator.getTotalHeuristic(WEIGHT);
+	        NewHeuristic stateEvaluator = new NewHeuristic(gameBoard, lastTop, top, rowsCleared);
+	        double score = stateEvaluator.getScore(WEIGHT);
 	        
 	        return score;
 	    }
