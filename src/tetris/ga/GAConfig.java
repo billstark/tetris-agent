1b2329d160a222c21fd31636d0089ec34c6f7a77
Wu Zefeng <flamesdesperado@gmail.com>
2018-04-03 16:20:49 +0800

    updated NUM_MOVES_IN_GAME

--- src/tetris/ga/GAConfig.java
+++ src/tetris/ga/GAConfig.java
@@ -7,7 +7,7 @@ public class GAConfig {
     public static final int NUM_GAMES_IN_ITERATION = 100;
 
     // Number of moves performed in each game
-    public static final int NUM_MOVES_IN_GAME = 500;
+    public static final int NUM_MOVES_IN_GAME = 2000;
 
     // Portion of population chosen for selecting the two fittest parents
     public static final double PORTION_PARENT_BATCH = 0.1;
